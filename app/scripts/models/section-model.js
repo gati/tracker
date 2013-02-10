89ff4bb096656e2cdd38d5a45a113f2642b64598
Jonathon Morgan <jonathon@goodattheinternet.com>
2013-02-09 20:02:39 -0600

    adding routes to section documents during bootstrap

--- app/scripts/models/section-model.js
+++ app/scripts/models/section-model.js
@@ -1,8 +1,17 @@
 Tracker.Section = Ember.Object.extend({
   name: null,
-  key: null
+  key: null,
+  shuttleRoutes: Ember.A()
 });
 
 Tracker.store.sections = Tracker.bootstrap.sections.map(function(section) {
-  return Tracker.Section.create(section);
+  var trackerSection = Tracker.Section.create(section);
+  
+  var shuttleRoutes = section.routes.map(function(route) {
+    return Tracker.store.shuttleRoutes.findProperty("key", route);
+  });
+
+  trackerSection.set("shuttleRoutes", shuttleRoutes);
+
+  return trackerSection;
 });
