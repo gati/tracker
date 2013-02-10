c364c870a7d369e675d3ffe4b82a940ac1b13285
Jonathon Morgan <jonathon@goodattheinternet.com>
2013-02-09 19:30:45 -0600

    removing ember-data

--- app/components/ember-data/packages/ember-data/lib/adapters.js
+++
@@ -1,2 +0,0 @@
-require("ember-data/adapters/fixture_adapter");
-require("ember-data/adapters/rest_adapter");
--- app/components/ember-data/packages/ember-data/lib/core.js
+++
@@ -1,4 +0,0 @@
-window.DS = Ember.Namespace.create({
-  // this one goes to 11
-  CURRENT_API_REVISION: 11
-});
